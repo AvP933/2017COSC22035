612b6339ecdac68d5b616e78d8c36f15581e681d
peramuna_ps14175 <avidarshana@gmail.com>
2017-12-13 14:48:54 +0530

    qws

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,12 @@
+import DBConnect.dbConnect;
+import sun.security.util.Password;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login {
     private JPanel Login;
@@ -38,10 +44,29 @@ public class Login {
 
     private void loginCheck(String frUsername,String frPW)
     {
-        String inUsername = "COSC";
-        String inPW = "22035";
+        String inUsername = null;
+        String inPW = null;
+        dbConnect dbcon = new dbConnect();
+        Statement stml = null;
+        try{
+            String query = "Select * userdetaills where UserName ='"+frUsername+"'";
+            stml = dbcon.Conn2DB();
+            ResultSet Rs = stml.executeQuery(query);
+            Rs.first();
+            inUsername = frUsername;
+            inPW = Rs.getString("Password");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+    }
+
+
+
+
         if(frUsername.equals(inUsername)&&frPW.equals(inPW)) {
             JOptionPane.showMessageDialog(null, "username or password correct");
+            loginFR.setVisible(false);
+            new userDetails(frUsername);
         } else
             JOptionPane.showMessageDialog( null,"Incorrect");
 
